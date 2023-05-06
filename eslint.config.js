1f685ce77c7b9d5eafe9b7cffeed69ac1b795e04
msga-mmm <michael.sga.mmm@gmail.com>
2023-05-06 16:52:19 -0500

    feat(linter): add new rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,35 +6,45 @@ import globals from "globals";
 const reactPreset = react.configs.recommended;
 
 export default [
-	{
-		files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
-		plugins: {
-			react,
-		},
-		languageOptions: {
-			parser: typescriptParser,
-			parserOptions: {
-				...reactPreset.parserOptions,
-			},
-			globals: {
-				...globals.browser,
-			},
-		},
-		rules: {
-			...reactPreset.rules,
-		},
-	},
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    plugins: {
+      react,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ...reactPreset.parserOptions,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      ...reactPreset.rules,
+    },
+  },
 
-	{
-		settings: {
-			react: {
-				version: "detect",
-			},
-		},
-		files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
-		rules: {
-			// react +17 doesn't need to have react imported
-			"react/react-in-jsx-scope": ["off"],
-		},
-	},
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    rules: {
+      // react +17 doesn't need to have react imported
+      "react/react-in-jsx-scope": ["off"],
+      "react/button-has-type": ["error"],
+      "react/default-props-match-prop-types": ["warn"],
+      "react/iframe-missing-sandbox": ["error"],
+      "react/jsx-max-depth": [
+        "warn",
+        {
+          max: 5,
+        },
+      ],
+      "react/jsx-no-constructed-context-values": ["error"],
+    },
+  },
 ];
