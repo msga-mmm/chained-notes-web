ff3bc582e1dc02a0a115e3247ee9838b2cdb8fa5
msga-mmm <michael.sga.mmm@gmail.com>
2023-05-06 02:02:36 -0500

    feat(linter): disable unnecessary react rule in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,36 +3,38 @@ import react from "eslint-plugin-react";
 import typescriptParser from "@typescript-eslint/parser";
 import globals from "globals";
 
-// presets: [all, recommended, jsx-runtime]
 const reactPreset = react.configs.recommended;
 
 export default [
-  {
-    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
-    plugins: {
-      react,
-    },
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        ...reactPreset.parserOptions,
-      },
-      globals: {
-        ...globals.browser,
-      },
-    },
-    rules: {
-      ...reactPreset.rules,
-    },
-  },
+	{
+		files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+		plugins: {
+			react,
+		},
+		languageOptions: {
+			parser: typescriptParser,
+			parserOptions: {
+				...reactPreset.parserOptions,
+			},
+			globals: {
+				...globals.browser,
+			},
+		},
+		rules: {
+			...reactPreset.rules,
+		},
+	},
 
-  {
-    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
-    rules: {
-      indent: ["error", "tab"],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
-    },
-  },
+	{
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+		files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+		rules: {
+			// react +17 doesn't need to have react imported
+			"react/react-in-jsx-scope": ["off"],
+		},
+	},
 ];
