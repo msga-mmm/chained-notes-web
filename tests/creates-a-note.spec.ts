5f71126a236bfc6cf3e063bda29fd17ef3b2e85d
msga-mmm <109926431+msga-mmm@users.noreply.github.com>
2024-03-31 15:08:43 -0500

    test(e2e): setup playwright tests (#57)
    * test(e2e): setup playwright tests
    
    * ci(e2e): setup HOME to /root for firefox e2e
    
    * chore(e2e): fail test to see output in ci
    
    * test(e2e): add failing test to see output in ci
    
    * Revert "test(e2e): add failing test to see output in ci"
    
    This reverts commit 44596f9c095d82715e5befca64c31e8aabdfbf35.
    
    * Revert "chore(e2e): fail test to see output in ci"
    
    This reverts commit 1b6a318ffd237734cd840f675b856c5989c079eb.

---
+++ tests/creates-a-note.spec.ts
@@ -0,0 +1,24 @@
+// TODO: avoid disabling eslint rule
+/* eslint-disable functional/no-expression-statements */
+import { test, expect } from "@playwright/test";
+
+test("creates a note", async ({ page }) => {
+  await page.goto("/");
+
+  const newNoteButton = page.getByRole("button", { name: /new note/i });
+  await newNoteButton.click();
+
+  await expect(page).toHaveURL(/notes\/\w+/);
+
+  const noteTitleTextbox = page.getByRole("textbox").getByText("untitled");
+
+  const noteTitle = "TODO list";
+  await noteTitleTextbox.fill(noteTitle);
+  await expect(page.getByText(noteTitle)).toHaveCount(2);
+
+  const noteBodyTextbox = page.getByRole("textbox").nth(1);
+
+  const noteBody = "- Buy pencils\n- Buy notebook\n- Organize bag";
+  await noteBodyTextbox.fill(noteBody);
+  await expect(page.getByText(noteBody)).toBeVisible();
+});
