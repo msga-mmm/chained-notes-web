f7fc21a9459086f1b640e3c7fd6ce95c5e27e900
msga-mmm <michael.sga.mmm@gmail.com>
2024-05-05 05:08:14 -0500

    test(e2e): add sign up step to authenticate test user

--- tests/creates-a-note.spec.ts
+++ tests/creates-a-note.spec.ts
@@ -1,24 +1,35 @@
 // TODO: avoid disabling eslint rule
 /* eslint-disable functional/no-expression-statements */
-import { test } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+import { test, expect } from "@playwright/test";
 
 test("creates a note", async ({ page }) => {
   await page.goto("/");
 
-  // const newNoteButton = page.getByRole("button", { name: /new note/i });
-  // await newNoteButton.click();
+  await page.getByRole("link", { name: "Sign up" }).click();
 
-  // await expect(page).toHaveURL(/notes\/\w+/);
+  await page
+    .getByLabel("Email address*")
+    .fill(faker.internet.email({ provider: "chained-notes.com" }));
+  await page.getByLabel("Password*").fill(faker.internet.password());
+  await page.getByRole("button", { name: "Continue", exact: true }).click();
 
-  // const noteTitleTextbox = page.getByRole("textbox").getByText("untitled");
+  await page.getByRole("button", { name: "Accept" }).click();
 
-  // const noteTitle = "TODO list";
-  // await noteTitleTextbox.fill(noteTitle);
-  // await expect(page.getByText(noteTitle)).toHaveCount(2);
+  const newNoteButton = page.getByRole("button", { name: /new note/i });
+  await newNoteButton.click();
 
-  // const noteBodyTextbox = page.getByRole("textbox").nth(1);
+  await expect(page).toHaveURL(/notes\/\w+/);
 
-  // const noteBody = "- Buy pencils\n- Buy notebook\n- Organize bag";
-  // await noteBodyTextbox.fill(noteBody);
-  // await expect(page.getByText(noteBody)).toBeVisible();
+  const noteTitleTextbox = page.getByRole("textbox").getByText("untitled");
+
+  const noteTitle = "TODO list";
+  await noteTitleTextbox.fill(noteTitle);
+  await expect(page.getByText(noteTitle)).toHaveCount(2);
+
+  const noteBodyTextbox = page.getByRole("textbox").nth(1);
+
+  const noteBody = "- Buy pencils\n- Buy notebook\n- Organize bag";
+  await noteBodyTextbox.fill(noteBody);
+  await expect(page.getByText(noteBody)).toBeVisible();
 });
